5f894a7b6174686c1ddbc02fbe13e9092f9cee70
HEINE Benjamin <benjamin.heine@ve-ict.net>
2020-07-17 14:42:46 +0200

    v05

--- tsmgen.ps1
+++ tsmgen.ps1
@@ -16,6 +16,8 @@ $DrivesDeviceNamesFile='windows_devices.txt'
 $DevicesCleanFile='windows_devices_clean.txt'
 $TSMCreatePathCommandFile='commands.txt'
 $PortWWN='41d4'
+$LibraryName='DD01'
+$SourceName='TSMLM_DMZ'
 
 
 Select-String -path $DrivesDeviceNamesFile -CaseSensitive -SimpleMatch -pattern  $PortWWN  | Select-String  -CaseSensitive -SimpleMatch -pattern "Changer" -NotMatch | select-object -ExpandProperty Line | out-file -FilePath $DevicesCleanFile
@@ -26,14 +28,14 @@ Get-Content $DrivesSerialFile | Select-Object -Skip 3 | Set-Content tsm_sn_drive
 
 foreach($Line in (get-content tsm_sn_drivename_skipped2lines.txt)) {
 	$SERIAL = $Line | Select-String -pattern  "S[0-9]{7,7}.." | % { $_.Matches } | % { $_.Value }
-    $DriveName = $Line | select-string 'drv\d\d' | % { $_.Matches } | % { $_.Value }
+    $DriveName = $Line | select-string 'drv\d+' | % { $_.Matches } | % { $_.Value }
 	Write-Output "Serial number is: $SERIAL - Drive name is: $DriveName"
 	$DEV_FILE_LINE=select-string -path $DevicesCleanFile -pattern $SERIAL 
 	if ( select-string -path $DevicesCleanFile -pattern $SERIAL)
 	 {
-		$DEV_PATH= $DEV_FILE_LINE | out-string | Select-String 'Tape[0-9][0-9]' |% { $_.Matches } | % { $_.Value }
+		$DEV_PATH= $DEV_FILE_LINE | out-string | Select-String 'Tape[0-9]+' |% { $_.Matches } | % { $_.Value }
 		write-output "Device is: $DEV_PATH"
-		write-output "define path ${DRIVE_NAME} device=$DEV_PATH"|Tee-object -filepath  $TSMCreatePathCommandFile -Append
+		write-output "define path $SourceName $DriveName srctype=Server destt=drive library=$LibraryName device=\\.\$DEV_PATH"|Add-content -path  $TSMCreatePathCommandFile 
 	 } 
 	 else
 	 {
